6c876483e16bbb724019b3835bdb03387251b78f
Lucas Fernandes <lucasdanieldmx@gmail.com>
2025-04-08 14:26:03 -0400

    s

--- combustivel.c
+++ combustivel.c
@@ -5,14 +5,6 @@ int main(void){
     /*declaração das variaveis, fui adicionando depois
     de passar o calculo pro papel apenas pra saber do que eu precisaria conforme fui codando*/
     float vel_media, temp_viagem, tanque_inicio, gasolina_necessaria, km_viagem, valor_total, abastecido_cidade, gasolina_pos_cidade;
-    
-    /*definição de constantes pros valores informados no inicion do exercicio
-    usei mais pra testar mesmo, mas os colegas disseram que é bom declarar constantes
-    por baa pratica quando o valor nao for mudar durante o codigo*/
-    const int autonomia_carro = 12;
-    const float gasolina_cidade = 4.80;
-    const float gasolina_fora = 5.20;
-    const int tam_tanque = 40;
 
     scanf("%f %f %f", &temp_viagem, &vel_media, &tanque_inicio);
 
@@ -20,22 +12,22 @@ int main(void){
     km_viagem = temp_viagem  * vel_media;
     
     /*descobrir quanto de gasolina ele vai usar pra poder viajar*/
-    gasolina_necessaria = km_viagem / autonomia_carro;
+    gasolina_necessaria = km_viagem / 12;
     
     /*decobrir quanto ele pode abastecer na cidade, ja que como é mais barato com certeza
     vai ser mais viavel sempre abastecer na cidade prmeiro kkkkkk, exeto ele esteja com o tanque cheio*/
-    abastecido_cidade = tam_tanque - tanque_inicio;
+    abastecido_cidade = 40 - tanque_inicio;
 
     /*if para verificar se ele consegue abastecer tudo na cidade, se conseguir nao precisa abastecer fora
     pra viajar e chegar com a mesma quantidade do tanque inicial*/
     if (gasolina_necessaria <= abastecido_cidade){
-        valor_total = gasolina_necessaria * gasolina_cidade;
+        valor_total = gasolina_necessaria * 4.80;
     }
     /*se ele nao conseguir fazer tudo com o abastecimento na cidade ele 
     tem que fazer paradas fora da cidade*/
     else{
         gasolina_pos_cidade = gasolina_necessaria - abastecido_cidade;
-        valor_total = (abastecido_cidade * gasolina_cidade) + (gasolina_pos_cidade * gasolina_fora);
+        valor_total = (abastecido_cidade * 4.80) + (gasolina_pos_cidade * 5.20);
     }
     /*finaliza mostrando o resultado final de acordo com a estrutura de dedscisao que o
     programa entrar*/
